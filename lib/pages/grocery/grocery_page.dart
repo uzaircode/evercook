c0bc898a42e3c6a04cd9a95c1b31efd169c3e8f5
Nik Uzair <nikuzairsc@gmail.com>
2024-03-31 15:02:00 +0800

    Setup bottom navigation bar with binding

---
+++ lib/pages/grocery/grocery_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GroceryPage extends StatelessWidget {
+  const GroceryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Grocery Page',
+        ),
+      ),
+    );
+  }
+}
