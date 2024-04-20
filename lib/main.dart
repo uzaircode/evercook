3aa1bcebf8e279ba4fbd071239bf47651a765755
Nik Uzair <nikuzairsc@gmail.com>
2024-04-20 19:25:07 +0800

    Add blocSelector from previous commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:evercook/core/theme/theme.dart';
 import 'package:evercook/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:evercook/features/auth/presentation/pages/login_page.dart';
 import 'package:evercook/init_dependencies.dart';
+import 'package:evercook/pages/home/home_page.dart';
 import 'package:evercook/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -48,7 +49,18 @@ class _MyAppState extends State<MyApp> {
       theme: AppTheme.themeData,
       // initialRoute: RoutesClass.getHomeRoute(),
       // getPages: RoutesClass.routes,
-      home: const LoginPage(),
+      home: BlocSelector<AppUserCubit, AppUserState, bool>(
+        selector: (state) {
+          return state is AppUserLoggedIn;
+        },
+        builder: (context, isLoggedIn) {
+          if (isLoggedIn) {
+            return const HomePage();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
     );
   }
 }
