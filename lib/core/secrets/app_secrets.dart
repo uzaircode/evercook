a150fbba7b868e2043216cb49aad22290e97b440
Nik Uzair <nikuzairsc@gmail.com>
2024-04-20 20:11:32 +0800

    Updated .gitignore to exclude app secrets

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://bvmromifulcyxstdjgzb.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bXJvbWlmdWxjeXhzdGRqZ3piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1OTIzMDcsImV4cCI6MjAyOTE2ODMwN30.A3AjhaBSNRwKqGTp4P-yMr8vlbQmtVcZld_Wvp3e5x0';
-}
