4a5cd8e13d3018cf84a0ee6e50e1e90cb8e6f6d1
Nik Uzair <nikuzairsc@gmail.com>
2024-04-24 08:23:15 +0800

    Initial features setup

--- lib/features/ingredient_wiki/data/datasources/ingredient_wiki_remote_data_source.dart
+++ lib/features/ingredient_wiki/data/datasources/ingredient_wiki_remote_data_source.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class IngredientRemoteDataSource extends StatelessWidget {
-  const IngredientRemoteDataSource({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
