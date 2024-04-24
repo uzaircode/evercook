f7a4a442b06e3bc6859ce4b321f9ca8ba794ec64
Nik Uzair <nikuzairsc@gmail.com>
2024-04-24 08:21:30 +0800

    Initial features setup

--- lib/features/recipe/presentation/widgets/ingredient_wiki_card.dart
+++ lib/features/recipe/presentation/widgets/ingredient_wiki_card.dart
@@ -1,4 +1,4 @@
-import 'package:evercook/features/ingredient/presentation/pages/ingredient_wiki_details.dart';
+import 'package:evercook/features/ingredient_wiki/presentation/pages/ingredient_wiki_details.dart';
 import 'package:flutter/material.dart';
 
 class IngredientWikiCard extends StatelessWidget {
